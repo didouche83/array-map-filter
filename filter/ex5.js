750f42bc4f188fb2a7a767344ec68998ee74b008
Widaad <mariage.barreto@gmail.com>
2018-10-11 22:18:31 +0200

    résolution exo filter 5

--- filter/ex5.js
+++ filter/ex5.js
@@ -38,8 +38,27 @@ Exemple d'entrée:
 
  */
 
-function searchWordFilter(items, search) {
+/*function searchWordFilter(items, search) {
+}*/
+
+const searchWordFilter = (items, search) => {
+  return items.filter((movie)=>{
+    let movieSmall = movie.toLowerCase()
+    let searchSmall = search.toLowerCase()
+    return movieSmall.includes(searchSmall)
+  })
 }
+
+/*console.log(searchWordFilter( [
+  'Mad Max: Fury Road',
+  'Interstellar',
+  'Revenge of the Nerds',
+  'Revenge of the Pink Panther',
+  'Star Wars: Episode I - The Phantom Menace',
+  'Star Wars: Episode II - Attack of the Clones',
+  'Star Wars: Episode III - Revenge of the Sith'
+], 'Revenge'
+))*/
 
 // Ne pas modifier l'export
 module.exports = searchWordFilter;
