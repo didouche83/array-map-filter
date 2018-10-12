a7de5c5e8259770ba22e7149cffae82c0c2abb9f
Widaad <mariage.barreto@gmail.com>
2018-10-12 09:00:05 +0200

    résolution exo 6 filter

--- filter/ex6.js
+++ filter/ex6.js
@@ -26,8 +26,25 @@ Sortie attendue:
 
 */
 
-function filterOffensiveComments(comments, bannedWords) {
+/*function filterOffensiveComments(comments, bannedWords) {
+}*/
+
+const filterOffensiveComments = (comments, bannedWords) => {  
+  return comments.filter((sentence)=>{
+    let sentenceSmall = sentence.toLowerCase();
+    return !sentenceSmall.includes(bannedWords[0]) && !sentenceSmall.includes(bannedWords[1]) 
+  })
 }
+
+console.log(filterOffensiveComments([
+  "Very useful tutorial, thank you so much!",
+  "React is not a damn framework, it's a LIBRARY",
+  "Why you put bloody kitten pictures in a tech tutorial is beyond me!",
+  "Which one is better, React or Angular?",
+  'There is no "better", it depends on your use case, DAMN YOU'
+], 
+  ['bloody', 'damn']
+))
 
 // Ne pas modifier l'export
 module.exports = filterOffensiveComments;
